a07b28c1abc7ce2da942d0f64d7671eaffb3aed6
Sandhya Pandey <168643341+sandhya144@users.noreply.github.com>
2024-05-21 16:15:29 +0000

    stack and heap

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -33,3 +33,28 @@ const myFunction = function(){
 }
 
 console.log(typeof bigNumber);  // we can check more ......
+
+
+// ******************************** Memory *********************************************
+
+// stack (Primitive) --> you'll get the copy of declared variable
+// and heap (non primitive type) -->  you'll get the reference of original value
+
+let myYoutubeChannel = "sandhyadotcom"
+let anothername = myYoutubeChannel
+anothername = "chaiaurcode"
+ 
+console.log(myYoutubeChannel);
+console.log(anothername);
+
+let userOne = {
+    email: "sandhya@google.com",
+    upi: "user@ybl",
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
