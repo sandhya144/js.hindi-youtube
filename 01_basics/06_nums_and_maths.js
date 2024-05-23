c0891d84237227ab3df3b01821dc627cda7de717
Sandhya Pandey <168643341+sandhya144@users.noreply.github.com>
2024-05-23 14:20:20 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,35 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);    // it specifies the number 
+
+// console.log(balance.toString().length);    //type of string
+// console.log(balance.toFixed(1));   // precision value
+
+const otherName = 123.8966
+// console.log(otherName.toPrecision(4)) 
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));  // to counts zeroes
+
+// ******************************* Maths ****************************************
+
+// console.log(Math);
+// console.log(Math.abs(-4));  // results positive value
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,5,6,7,8));
+// console.log(Math.max(7,8,9));
+
+
+console.log(Math.random());  // it can take values from  0 to 1
+console.log((Math.random()*10) + 1);  // add +1 to avoid that it can't gets the value 0  (0.04) ---> floor returns 0
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min + 1)) + min)
+
