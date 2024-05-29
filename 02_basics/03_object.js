fbb7d4d515b70243d504b23102ac49de853430c6
Sandhya Pandey <168643341+sandhya144@users.noreply.github.com>
2024-05-29 18:31:03 +0000

    object

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -1,19 +1,28 @@
-// singelton  --> creates single object 
+// singelton  --> ye apne tarah ka ek hi object hai  
 // singelton forms when we use constructor not in literals
 
 // Object.create  constructor methods
 
 // object literals 
-// object declaration
+// object declaration  --> keys and values
+const mySym = Symbol("key1") 
+
 const JsUser = {
     name: "hitesh",
+    "full name": "Hitesh Chaudhary",
+    [mySym]: "mykey1",
     age: 18,
     location: "Jaipur",
     email: "hitesh@google.com",
     isLoggedIn: false,
     lastLoginDays: ["monday", "saturday"]
 } 
+
 //  to access an object 
 console.log(JsUser.email)
-console.log(JsUser["email"])
+console.log(JsUser["email"])   // email treats --> string
+console.log(JsUser["full name"])   // dot  --> can't access
+console.log(JsUser[mySym]) 
+
+
 
