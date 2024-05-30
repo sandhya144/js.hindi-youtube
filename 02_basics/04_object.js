d52243f20848c737102561861bcb3e8e1bbc7f08
Sandhya Pandey <168643341+sandhya144@users.noreply.github.com>
2024-05-30 14:38:15 +0000

    obj

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -3,6 +3,24 @@ const tinderUser = {}
 
 tinderUser.id = "123abc"
 tinderUser.name = "Sandy"
-tinderUser.idLoggedIn = false 
+tinderUser.isLoggedIn = false 
 
-console.log(tinderUser);
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname: "sandhya",
+            lastname: "pandey"
+        }
+    }
+}
+// console.log(regularuser.fullname.userfullname.firstname.lastname);  // methods to access 
+// console.log(regularuser.fullname?.userfullname.firstname.lastname);   // optional chaining
+
+// obj combination
+const obj1 = {1: "a",2: "b"}
+const obj2 = {3: "a",4: "b"}
+const obj3 = {obj1 , obj2}
+console.log(obj3);
