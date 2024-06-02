2a9fc55c4c7c7d669a6f7d96ada3b6ac612eac4e
Sandhya Pandey <168643341+sandhya144@users.noreply.github.com>
2024-06-02 12:20:09 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,13 +1,13 @@
-var c = 300    
-// {} --> with some function or array called as scope
-
+// var c = 300    
+// {} --> with some function or if else called as scope
 let a = 300
 if(true){
     let a = 10
     const b = 20      // inside if --> block scope --> can't go outside ; outside it --> global scope --> value available for block scope
-    var c = 30    // returns -->30
+    // var c = 30    // returns -->30
+    console.log("INNER: ", a);
 }    
 
-// console.log(a);
+console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
