7cfbce6d68c47110df92afcd3548cea33d638429
Sandhya Pandey <168643341+sandhya144@users.noreply.github.com>
2024-06-02 13:21:23 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -8,6 +8,45 @@ if(true){
     console.log("INNER: ", a);
 }    
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+/******************nested scope*****************/
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);   // line by line execution 
+
+    two()
+
+}
+
+one()
+
+if (true){
+    const username = "hitesh"
+    if (username == "hitesh"){
+       const website = "youtube"
+       console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// ********************interesting**********************//
+addone(5)
+function addone(num){
+ return num + 1
+}
+// console.log(addone(5))
+
+addTwo(5)   // cannot access before initialisation
+const addTwo = function(num){
+  return num + 2
+}
+console.log(addTwo(5));
