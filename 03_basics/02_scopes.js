83912f0620c12464260459f4cc0f50225eba91eb
Sandhya Pandey <168643341+sandhya144@users.noreply.github.com>
2024-06-02 10:58:42 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,6 +1,7 @@
 var c = 300    
-
 // {} --> with some function or array called as scope
+
+let a = 300
 if(true){
     let a = 10
     const b = 20      // inside if --> block scope --> can't go outside ; outside it --> global scope --> value available for block scope
