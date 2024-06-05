4232a1bb512a86d1d270d2215fe9da1322d0d179
Sandhya Pandey <168643341+sandhya144@users.noreply.github.com>
2024-06-05 13:19:54 +0000

    one

---
+++ 04_life.js/one.js
@@ -0,0 +1,18 @@
+// if (true--> code will execute) & (false --> code will not execute)
+const isUserLoggedIn = true
+
+if (2 != 3){
+//   console.log("executed");
+}
+// <,>,<=,>=,==,!=,===,!==
+
+// const temperature = 42
+// if(temperature < 50){
+//     console.log("less than 50");
+// } else{                                  // conditional checking
+// console.log("temperature is greater than 50")
+// }
+// console.log("execute");
+
+
+
