d5f9ffc71767d98d76cca058beb6c9673faaeca3
Sandhya Pandey <168643341+sandhya144@users.noreply.github.com>
2024-06-08 00:57:53 +0000

    five

---
+++ 05_iterators/05_five.js
@@ -0,0 +1,41 @@
+// high order array loops...... (we directly calls the function)
+
+const coding = ["js","ruby","java","python","cpp"]
+
+// coding.forEach (function (val){          
+//     console.log(val);
+// })
+
+// coding.forEach ( (item) => {
+//     console.log(item);
+// })
+
+function printMe(item){
+    console.log(item);
+}
+// coding.forEach(printMe)               //reference
+
+// coding.forEach ( (item, index, arr) => {
+//         console.log(item, index, arr);
+//     })
+
+    const myCoding = [
+        {
+         languageName: "java",
+         languageFileName: "java"   
+        },
+
+        {
+        languageName: "javaScript",
+        languageFileName: "js"   
+        },
+
+        {
+        languageName: "python",
+        languageFileName: "py"   
+        }
+    ]
+
+    myCoding.forEach( (item) =>
+    console.log(item.languageName)
+    )
