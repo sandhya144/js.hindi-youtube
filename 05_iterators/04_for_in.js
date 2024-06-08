3cca10adbbb081c2fbc257f96a7db3725f6cd9c0
Sandhya Pandey <168643341+sandhya144@users.noreply.github.com>
2024-06-08 00:12:16 +0000

    for in

---
+++ 05_iterators/04_for_in.js
@@ -0,0 +1,27 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb:'ruby',
+    swift: 'swift by apple',
+
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+    }
+
+const programming = ["js","rb","py","java","cpp"]
+for (const key in programming) {
+    // console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', "INDIA")
+// map.set('USA',"United states of america")    // it's not iteratable
+// map.set('Fr',"France")
+// map.set('IN', "INDIA")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
